938ca5b9d59fdfee6d40ccb1de589b924dd50969
Marco Fugaro <marco.fugaro@gmail.com>
2017-06-13 18:47:34 +0200

    Add an example link to test component

--- src/Test.js
+++ src/Test.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
 
+@inject('routing')
+@observer
 export default class Test extends Component {
+  navigateHome = (e) => {
+    e.preventDefault()
+    const { push } = this.props.routing
+    push('/')
+  }
   render() {
     return (
-      <div>Test component</div>
+      <div>Test component, <a onClick={this.navigateHome}>Back to home!</a></div>
     )
   }
 }
