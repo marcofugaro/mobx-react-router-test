23afc416bb63f7560b9803f320e7561aa3c2d7b3
Marco Fugaro <marco.fugaro@gmail.com>
2017-06-13 18:38:44 +0200

    First commit with mobx-react-router example

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
+
+@inject('routing')
+@observer
+export default class App extends Component {
+  render() {
+    const { location, push, goBack } = this.props.routing
+
+    return (
+      <div>
+        <span>Current pathname: {location.pathname}</span>
+        <button onClick={() => push('/test')}>Change url</button>
+        <button onClick={() => goBack()}>Go Back</button>
+      </div>
+    )
+  }
+}
